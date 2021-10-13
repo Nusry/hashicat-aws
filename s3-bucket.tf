0a694bbf4899e5b2a5bc5921d645030d5a8dc91c
Nusry <mnusry@gmail.com>
2021-10-13 05:24:39 +0000

    Added s3 bucket module versio1n1ddadfd3

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,6 +2,6 @@ module "s3_bucket" {
   source  = "app.terraform.io/gesbpo/s3-bucket/aws"
   version = "2.2.0"
   bucket_prefix = "mnusry"
-  bucket = "bucket_32325_testing_nusry_adfa"
+  bucket = "bucket_32325_testing_adfa"
   acl    = "private"
 }
